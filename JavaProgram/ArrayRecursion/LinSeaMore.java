01eb311a155c32e48b8087b7f355ba6d53305126
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-09-01 23:47:07 +0530

    Multiple occurences in linear search

--- JavaProgram/ArrayRecursion/LinSeaMore.java
+++ JavaProgram/ArrayRecursion/LinSeaMore.java
@@ -1,7 +1,29 @@
 package JavaProgram.ArrayRecursion;
-
+import java.util.*;
 public class LinSeaMore {
     public static void main(String[] args) {
         
-    }
-}
+                int[] arr = {2, 3, 1, 4, 4, 5};
+        
+                int target = 4;
+        
+                ArrayList<Integer> ans = findAll(arr, target);
+        
+                System.out.println(ans);
+            }
+        
+            static ArrayList<Integer> findAll(int[] arr, int target) {
+        
+                ArrayList<Integer> list = new ArrayList<>();
+        
+                for (int i = 0; i < arr.length; i++) {
+        
+                    if (arr[i] == target) {
+                        list.add(i);
+                    }
+                }
+        
+                return list;
+            }
+        }
+

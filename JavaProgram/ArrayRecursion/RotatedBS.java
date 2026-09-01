5422dd0f45d57d1f5326e90b8a62f9003261c7d4
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-09-01 23:53:49 +0530

    Rotated Binary Search used recursion

--- JavaProgram/ArrayRecursion/RotatedBS.java
+++ JavaProgram/ArrayRecursion/RotatedBS.java
@@ -3,23 +3,17 @@ package JavaProgram.ArrayRecursion;
 public class RotatedBS {
 
     public static void main(String[] args) {
-
         int[] arr = {5, 6, 7, 8, 9, 1, 2, 3};
-
         int target = 2;
-
         int ans = search(arr, target, 0, arr.length - 1);
-
         System.out.println(ans);
     }
 
     static int search(int[] arr, int target, int s, int e) {
-
         if (s > e) {
             return -1;
         }
         int m = s + (e - s) / 2;
-
         if (arr[m] == target) {
             return m;
         }
